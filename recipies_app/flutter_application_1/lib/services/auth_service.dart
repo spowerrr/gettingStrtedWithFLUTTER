7681eeac558fba6da40d0a954a5d4c391bfa423d
spowerrr <mpuspo2310304@bscse.uiu.ac.bd>
2024-11-28 22:21:51 +0600

    updated

--- recipies_app/flutter_application_1/lib/services/auth_service.dart
+++ recipies_app/flutter_application_1/lib/services/auth_service.dart
@@ -1,11 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_application_1/services/http_sevice.dart';
+
 class AuthService {
   static final AuthService _singleton = AuthService.internal();
+  final _httpService = HTTPService();
   factory AuthService() {
     return _singleton;
   }
   AuthService.internal();
   Future<bool> login(String username, String passward) async {
-    print(username);
+    //print(username);
+    try {
+      var response = await _httpService.post("auth/login", {
+        "username": username,
+        "password": passward,
+      });
+      //print(response?.statusCode);
+      if (response?.statusCode == 200 && response?.data != null) {
+        print(response!.data);
+      }
+    } catch (e) {
+      print(e);
+    }
     return false;
   }
 }
--- recipies_app/flutter_application_1/lib/services/http_sevice.dart
+++ recipies_app/flutter_application_1/lib/services/http_sevice.dart
@@ -1,11 +1,39 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_application_1/consts.dart';
+
 class HTTPService {
   static final HTTPService _singleton = HTTPService.internal();
   factory HTTPService() {
     return _singleton;
   }
   final _dio = Dio();
-  HTTPService.internal(){
-    setup();
-  };
-  Future<void> setup(String? bearerToken)async{}
+  HTTPService.internal() {
+    setup(null);
+  }
+  Future<void> setup(String? bearerToken) async {
+    final headers = {
+      "Content-Type": "application/json",
+    };
+    final options = BaseOptions(
+      baseUrl: API_BASE_URL,
+      headers: headers,
+      validateStatus: (status) {
+        if (status == null) return false;
+        return status < 500;
+      },
+    );
+    _dio.options = options;
+  }
+
+  Future<Response?> post(String path, Map data) async {
+    try {
+      final response = await _dio.post(
+        path,
+        data: data,
+      );
+      return response;
+    } catch (e) {
+      print(e);
+    }
+  }
 }
